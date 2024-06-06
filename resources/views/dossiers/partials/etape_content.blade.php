335df93ae9d5a9c6c0f795c5ea2b6c1d0b5b037c
Lazzzzz <37779417+Lazzzzz@users.noreply.github.com>
2024-06-06 04:01:21 +0200

    Update etape_content.blade.php

--- resources/views/dossiers/partials/etape_content.blade.php
+++ resources/views/dossiers/partials/etape_content.blade.php
@@ -7,7 +7,7 @@
             <h3 class="border-bottom border-gray pb-2">{{ $etape->etape_desc }}</h3>
         </div>
         @dd($dossier)
-        @if ($etape->etape_number == $dossier->etape_number && $dossier->status_id != 15)
+        @if ($etape->etape_number == $dossier->etape_number && $dossier->status->status_name != "Refusé")
             <div class="col-lg-6">
                 <a class="btn btn-primary" href="{{ route('dossiers.next_step', $dossier->id) }}">Valider l'étape</a>
             </div>
