cf47ccb17f0b74136768ad3cd39b279697482fa1
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-06-08 17:51:36 +0200

    Update dossier-livewire.blade.php

--- resources/views/livewire/dossier-livewire.blade.php
+++ resources/views/livewire/dossier-livewire.blade.php
@@ -116,7 +116,8 @@
                                             <tbody>
                                                 @foreach ($forms_configs as $index => $form_handler)
                                                     @if ($form_handler->form->etape_number == $tab && $form_handler->form->type == 'document')
-                                                        {!! $form_handler->render([]) !!} <!-- Render without error array -->
+                                                    @dump($form_handler)   
+                                                    {!! $form_handler->render([]) !!} <!-- Render without error array -->
                                                     @endif
                                                 @endforeach
                                             </tbody>
