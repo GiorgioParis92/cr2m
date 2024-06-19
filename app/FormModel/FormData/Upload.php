58c9ade36196c55a826d7a4a46a1d9920586a541
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-06-20 00:24:45 +0200

    Update Upload.php

--- app/FormModel/FormData/Upload.php
+++ app/FormModel/FormData/Upload.php
@@ -30,7 +30,7 @@ class Upload extends AbstractFormData
 
         $data .= '</td>';
 
-        $data .= '<td class="w-30" ><div wire:poll.visible>';
+        $data .= '<td class="w-30" ><div >';
 
         $extension = explode('.', $this->value);
 
