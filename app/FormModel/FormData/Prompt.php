99af8a610b2e8d2e0566ba87a65d1083d9fa3f87
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-06-19 14:57:17 +0200

    Update Prompt.php

--- app/FormModel/FormData/Prompt.php
+++ app/FormModel/FormData/Prompt.php
@@ -16,7 +16,7 @@ class Prompt extends AbstractFormData
 
         $data = '<div style="margin-bottom:20px" class="form-group  col-sm-12 '.($this->config->class ?? "").'">';
         $data .= '<label>'.$this->config->title.'</label>';
-        $data .= '<textarea  style="min-height:300px" class="form-control" name="'.$this->config->name.'">'.$optionsArray['prompt'].'</textarea>';
+        $data .= '<textarea  style="min-height:300px" class="form-control" name="'.$this->config->name.'">'.nl2br($optionsArray['prompt']).'</textarea>';
 
         $data .= '</div>';
 
