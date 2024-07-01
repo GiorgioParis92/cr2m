f60ea0dbdbb3931abd07681ff621c6acc9cc8a07
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-07-01 02:41:05 +0200

    Update Condition.php

--- app/FormModel/FormData/Condition.php
+++ app/FormModel/FormData/Condition.php
@@ -29,7 +29,7 @@ class Condition extends AbstractFormData
             $this->value='';
             $this->save_value();
         }
-        $data = '<input wire:model.lazy="' . $wireModel . '" value="'.$this->value.'" id="' . $this->name . '"  class="form-control" type="text" name="' . $this->name . '"';
+        $data = '<input wire:model.lazy="' . $wireModel . '" value="'.$this->value.'" id="' . $this->name . '"  class="form-control" type="text" name="' . $this->name . '">';
 
 
 
