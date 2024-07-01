bf71f55a29c75efe5e8053adf9f5cc64c75508e8
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-07-01 02:45:37 +0200

    Update Condition.php

--- app/FormModel/FormData/Condition.php
+++ app/FormModel/FormData/Condition.php
@@ -29,11 +29,11 @@ class Condition extends AbstractFormData
             $this->value='';
             $this->save_value();
         }
-        $data = '<input wire:model.lazy="' . $wireModel . '" value="'.$this->value.'" id="' . $this->name . '"  class="form-control" type="hidden" name="' . $this->name . '">';
+        $data = '<input wire:model.lazy="' . $wireModel . '" value="'.$this->value.'" id="' . $this->name . '"  class="form-control" type="text" name="' . $this->name . '">';
 
 
         if($this->value=='error') {
-            $data .= '<div  class="invalid-feedback">Erreur</div>';
+            $data .= '<div style="display:block" class="invalid-feedback">Erreur</div>';
         }
 
 
