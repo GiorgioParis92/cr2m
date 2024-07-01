9f13a2743d23f968eae85f6dfe58ee609296b5ce
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-07-01 12:32:22 +0200

    Update navbar.blade.php

--- resources/views/frontend/navbar.blade.php
+++ resources/views/frontend/navbar.blade.php
@@ -69,8 +69,8 @@
                       <span class="sidenav-normal"><i class="fa fa-comments"></i>  Discussions </span>
                   </a>
               </li>
-@dump(auth()->user())
-                @if (auth()->user()->type_id == 1)
+
+              @if (auth()->user()->type_id == 1)
                     <li class="nav-item">
                         <a class="nav-link  " href="{{ route('users.index') }}">
 
