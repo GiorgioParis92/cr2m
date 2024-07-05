3e92382ca932f216e630b2abf5364e37b16eaf9d
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-07-05 11:48:42 +0200

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -139,9 +139,9 @@ if (!function_exists('format_date')) {
 if (!function_exists('strtotime_date')) {
     function strtotime_date($value)
     {
-        $value = str_replace('/', '-', $value);
-        $value = str_replace('.', '-', $value);
-        $value = str_replace('', '-', $value);
+        // $value = str_replace('/', '-', $value);
+        // $value = str_replace('.', '-', $value);
+        // $value = str_replace('', '-', $value);
         $value = strtotime($value);
         return $value;
     }
