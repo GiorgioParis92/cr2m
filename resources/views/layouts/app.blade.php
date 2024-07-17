8ceef1b0d5c9a32afd86fb9a19d4f0b4157e020f
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-07-17 14:28:01 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -602,6 +602,34 @@
     function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
+
+    $('.delete_photo').click(function() {
+            alert('stop')
+            console.log($('meta[name="_token"]').attr('content'))
+            var link = $(this).data('val');
+            $.ajax({
+                url: '/delete_file',
+                method: 'POST',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+                },
+                data: {
+                    link: link,
+
+                },
+                success: function(response) {
+
+
+                },
+                error: function(xhr) {
+                    let errorMessage = 'An error occurred';
+                    if (xhr.responseJSON && xhr.responseJSON.errors) {
+                        errorMessage = Object.values(xhr.responseJSON.errors).join(', ');
+                    }
+
+                }
+            });
+        })
 </script>
 
 
