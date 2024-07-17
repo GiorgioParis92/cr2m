f84496942ff734589f8716aced3d511ed8b0abe4
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-07-17 14:14:43 +0200

    Update Photo.php

--- app/FormModel/FormData/Photo.php
+++ app/FormModel/FormData/Photo.php
@@ -89,31 +89,7 @@ class Photo extends AbstractFormData
                 }
             });
         
-            $('.delete_photo').click(function(){
-                var link=$(this).data('val');
-                $.ajax({
-            url: '{$deleteUrl}',
-            method: 'POST',
-                headers: {
-                    'X-CSRF-TOKEN': '{$csrfToken}'
-                },
-            data: {
-                    link: link,
-                   
-                },
-            success: function(response) {
-          
-    
-            },
-            error: function(xhr) {
-                let errorMessage = 'An error occurred';
-                if (xhr.responseJSON && xhr.responseJSON.errors) {
-                    errorMessage = Object.values(xhr.responseJSON.errors).join(', ');
-                }
-        
-            }
-        });
-            })
+ 
         
         </script>";
         if ($this->value) {
