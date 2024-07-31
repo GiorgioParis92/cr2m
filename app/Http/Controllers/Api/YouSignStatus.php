56de1d17c645b4866be7936d94e0775a71f6362d
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-07-31 12:55:45 +0200

    Update YouSignStatus.php

--- app/Http/Controllers/Api/YouSignStatus.php
+++ app/Http/Controllers/Api/YouSignStatus.php
@@ -248,7 +248,7 @@ class YouSignStatus extends Controller
               $url = $responseData->result->data->url_info->url;
               $token = $responseData->result->data->url_info->token; // Replace YOUR_BEARER_TOKEN with your actual token
 
-              $path = 'storage/dossiers/'.$request->dossier_id.'/'.$request->template.'_signee.pdf';
+              $path = 'storage/dossiers/'.$request->dossier_id.'/'.$request->template.'.pdf';
 
               $outputFile = public_path($path);
 
