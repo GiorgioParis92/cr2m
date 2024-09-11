59910ba37242c83a996865f15a9f49194c639073
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-09-11 11:30:07 +0200

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -74,7 +74,7 @@ if (!function_exists('is_user_allowed')) {
 
             $defaultPermission = DB::table('default_permission')->where('type_id', $user->type_id)
                 ->where('permission_name', $permission_name)
-                ->where('type_client', '>', 0)
+                ->where('type_client', $user->client->type_client)
 
                 ->first();
 
