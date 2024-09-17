7862f3264618c80e623bbaa0a35d2bffdc5adb5e
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-09-17 15:34:58 +0200

    Update dashboard.blade.php

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -828,7 +828,7 @@
                 </div>
 
                 <div class="col-sm-12">
-                    <div class="card h-100">
+                    <div class="card ">
                         <div class="card-header pb-0 p-3">
                             <div class="row">
                                 <div class="col-md-6">
