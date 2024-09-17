a826bc2ab90fa0661abfb740f6463d92af822f3c
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-09-17 16:20:41 +0200

    Update ClientLinks.php

--- app/Models/ClientLinks.php
+++ app/Models/ClientLinks.php
@@ -10,7 +10,11 @@ class ClientLinks extends Model
     use HasFactory;
 
     protected $table = 'clients_links';
-
+    protected $fillable = [
+        'client_id',      // Add client_id to the fillable array
+        'client_parent',  // Add client_parent or other fields you use in mass assignment
+        // Add any other fields that should be mass assignable
+    ];
     public function client_parent()
 {
     return $this->belongsTo(Client::class, 'client_parent','id');
