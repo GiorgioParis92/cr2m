17c13ca36e4ffab7d6ed879b58a6fa61c8f65bf4
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-09-22 20:08:59 +0200

    Update DossierLivewire.php

--- app/Http/Livewire/DossierLivewire.php
+++ app/Http/Livewire/DossierLivewire.php
@@ -73,6 +73,7 @@ class DossierLivewire extends Component
         }
         $last_etape = 1;
         foreach ($this->etapes as $etape) {
+            dump(($etape->order_column) + 1);
             if (is_user_allowed($etape->etape_name) == true && (($etape->order_column) + 1) <= $this->dossier['etape_number']) {
                 $last_etape = ($etape->id);
                 dump($last_etape);
