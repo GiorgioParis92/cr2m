e8ee2a59d760359efcb0b08a56b46f0e669a693d
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-09-25 12:50:32 +0200

    Update Title.php

--- app/FormModel/FormData/Title.php
+++ app/FormModel/FormData/Title.php
@@ -83,8 +83,8 @@ class Title extends AbstractFormData
     {
 
 
-        // $data= '<div class="col-12 form_title s2">'.($this->config->title ?? '').'</div>';
-        $data= ($this->config->title ?? '');
+        $data= '<div class="col-12 form_title s2">'.($this->config->title ?? '').'</div>';
+        // $data= ($this->config->title ?? '');
 
 
 
