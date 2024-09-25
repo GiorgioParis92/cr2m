83c15d42ea4db0ea0de1c28bfd09a7971747b998
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-09-25 16:24:27 +0200

    Update Table.php

--- app/FormModel/FormData/Table.php
+++ app/FormModel/FormData/Table.php
@@ -177,7 +177,7 @@ class Table extends AbstractFormData
     {
         
         $should_render = false;
-        $data = '<div>';
+        $data = '<div><table>';
     
         // Decode the JSON value if needed
         $this->value = $this->decode_if_json($this->value);
@@ -209,13 +209,13 @@ class Table extends AbstractFormData
                 
        
               
-                $data = '<table><tr><td>';
+                $data .= '<tr><td>';
                 $data .= $instance->render_pdf();
-                $data = '</td></tr></table>';
+                $data .= '</td></tr>';
        
             }
         }
-        $data = '</div>';
+        $data .= '</table></div>';
         // print_r($data);
         // return false;
         return $data;
