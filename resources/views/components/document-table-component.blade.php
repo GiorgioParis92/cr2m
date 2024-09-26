e272824f137793f3930e83f3dcca4c3b78ff96c1
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-09-27 00:05:31 +0200

    Update document-table-component.blade.php

--- resources/views/components/document-table-component.blade.php
+++ resources/views/components/document-table-component.blade.php
@@ -1,43 +1,43 @@
 @props(['docs'])
 @foreach ($docs as $doc)
 
-    @if($doc['required']==1 || ($doc['required']==0 && isset($doc['meta_value']) && !empty($doc['meta_value'])))
+    @if ($doc['required'] == 1 || ($doc['required'] == 0 && isset($doc['meta_value']) && !empty($doc['meta_value'])))
 
-    @php $data=[] @endphp
+        @php $data=[] @endphp
 
-    @if (isset($doc['meta_value']) && $doc['meta_value'] != null)
-        @php $color ='success' @endphp
-    @else
-        @php $color ='danger' @endphp
-    @endif
-
-
-    @if (isset($doc['options']))
-        @if (isset($doc['options']['signable']) && $doc['options']['signable'] == 'true')
-            @php $data=($doc['additional_data']) @endphp
+        @if (isset($doc['meta_value']) && $doc['meta_value'] != null)
+            @php $color ='success' @endphp
+        @else
             @php $color ='danger' @endphp
+        @endif
 
-            @if (!empty($data) && isset($doc['signature_request_id']))
-                @if (isset($doc['signature_status']))
-                    @if ($doc['signature_status'] == 'finish')
-                        @php $color='success' @endphp
-                    @endif
-                    @if ($doc['signature_status'] == 'ongoing')
-                        @php $color='warning' @endphp
+
+        @if (isset($doc['options']))
+            @if (isset($doc['options']['signable']) && $doc['options']['signable'] == 'true')
+                @php $data=($doc['additional_data']) @endphp
+                @php $color ='danger' @endphp
+
+                @if (!empty($data) && isset($doc['signature_request_id']))
+                    @if (isset($doc['signature_status']))
+                        @if ($doc['signature_status'] == 'finish')
+                            @php $color='success' @endphp
+                        @endif
+                        @if ($doc['signature_status'] == 'ongoing')
+                            @php $color='warning' @endphp
+                        @endif
                     @endif
                 @endif
-            @endif
-            @if(!empty($data) && !isset($doc['signature_request_id']))
-                @php $color='danger' @endphp
-            @endif
+                @if (!empty($data) && !isset($doc['signature_request_id']))
+                    @php $color='danger' @endphp
+                @endif
 
+            @endif
         @endif
-    @endif
 
-    <div class="btn btn-{{ $color }} btn-view @if (isset($doc['meta_value']) && !empty($doc['meta_value'])) pdfModal @endif"
-        data-toggle="modal" data-img-src="{{ asset('storage/' . $doc['meta_value']) }}?time=1727288485"
-        data-name="Fiche navette">
-        <i class="fas fa-eye"></i> {{ $doc['title'] }}
-    </div>
+        <div class="btn btn-{{ $color }} btn-view @if (isset($doc['meta_value']) && !empty($doc['meta_value'])) pdfModal @endif"
+            @if (isset($doc['meta_value']) && !empty($doc['meta_value'])) data-toggle="modal" data-img-src="{{ asset('storage/' . $doc['meta_value']) }}?time=1727288485"
+        data-name="Fiche navette" @endif>
+            <i class="fas fa-eye"></i> {{ $doc['title'] }}
+        </div>
     @endif
 @endforeach
