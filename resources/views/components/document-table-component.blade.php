f26c6fb5f29142a95bbf4014f8b73e1b4f1d7a27
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-09-27 00:08:44 +0200

    Update document-table-component.blade.php

--- resources/views/components/document-table-component.blade.php
+++ resources/views/components/document-table-component.blade.php
@@ -32,7 +32,7 @@
                 @endif
                 @if (!empty($data) && !isset($doc['signature_request_id']))
                     @php $color='danger' @endphp
-                    @php $text="\n".'Non signé' @endphp
+                    @php $text="\n".'<br/>Non signé' @endphp
                 @endif
 
             @endif
