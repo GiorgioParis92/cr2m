d936e7af8531fd2ec5f1b1b9e97373ecb8a69fba
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-09-30 13:43:00 +0200

    Update Result.php

--- app/FormModel/FormData/Result.php
+++ app/FormModel/FormData/Result.php
@@ -293,4 +293,250 @@ class Result extends AbstractFormData
     
         return true;
     }
+
+
+
+    public function render_pdf(bool $is_error)
+    {
+
+
+
+        $jsonString = str_replace(["\n", '', "\r"], '', $this->config->options);
+        $optionsArray = json_decode($jsonString, true);
+
+        if (isset($optionsArray['arguments'])) {
+            foreach ($optionsArray['arguments'] as $key => $data) {
+                $sql_command = str_replace($key, eval ($data), $sql_command);
+            }
+        }
+
+        $condition_valid = false;
+
+        if(isset($optionsArray['conditions'])) {
+          
+            foreach ($optionsArray as $key=>$condition_config) {
+                if($key=='conditions') {
+                    if ($this->check_condition($condition_config)) {
+                
+                        $condition_valid = true;
+        
+                        if (isset($condition_config['operation']) && $condition_config['operation'] == 'AND') {
+                            break;
+                        }
+        
+                    } else {
+                        $condition_valid = false; 
+                    }
+                }
+
+    
+            }
+        } else {
+            $condition_valid = true;
+        }
+
+
+  
+        if( $condition_valid == false) {
+            return '';
+        }
+        if (isset($optionsArray['operands'])) {
+            foreach ($optionsArray['operands'] as $operand) {
+
+                if ($operand['operand'] == 'x') {
+                
+                 
+                    if(!isset($total) ) {
+                        $total=1;
+                      
+                    }
+                  
+
+                    
+                    foreach ($operand['tags'] as $tag) {
+                        $tagValue = $this->getOtherValue($tag);
+                   
+
+                        if ($tagValue === '' || $tagValue === null) {
+                            if (is_numeric($tag)) {
+                                $tagValue = $tag;
+                            } else {
+                                $tagValue = 0;
+                            }
+                        }
+
+                        $total *= $tagValue;
+                    }
+                  
+                }
+
+
+         
+                if ($operand['operand'] == '/') {
+         
+                    foreach ($operand['tags'] as $tag) {
+                        if(!isset($total)) {
+                            $total=0;
+                        }
+                      
+                        $tagValue = $this->getOtherValue($tag) ? number_format((float) $this->getOtherValue($tag), 2, '.', '') : '';
+
+                        
+                        if ($tagValue === '' || $tagValue === null ) {
+
+                            if (is_numeric($tag)) {
+                                $tagValue = $tag;
+                                
+                            } else {
+                                $tagValue = 1;
+                            }
+                        }
+                        if ($tagValue != 0) {
+                            $total /= $tagValue;
+                        }
+
+                    }
+                  
+                   
+                }
+
+                if ($operand['operand'] == '-') {
+                 
+                    if(!isset($total)) {
+                        $total=0;
+                    }
+                  
+                    foreach ($operand['tags'] as $tag) {
+                        $tagValue = number_format((float) $this->getOtherValue($tag), 2, '.', '');
+
+                        if ($tagValue === '' || $tagValue === null) {
+                            if (is_numeric($tag)) {
+                                $tagValue = $tag;
+                            } else {
+                                $tagValue = 1;
+                            }
+                        }
+                 
+                        
+                        $total -= $tagValue;
+                    }
+                }
+
+                if ($operand['operand'] === '+') {
+                   
+                    if(!isset($total)) {
+                        $total=0;
+                    }
+                    
+                    foreach ($operand['tags'] as $tag) {
+
+                        $tagValue = number_format((float) $this->getOtherValue($tag), 2, '.', '');
+
+
+                        $total += $tagValue;
+                    }
+                }
+                if ($operand['operand'] === '<') {
+                   
+                    if(!isset($total)) {
+                        $total=0;
+                    }
+                    
+                    foreach ($operand['tags'] as $tag) {
+
+                        $tagValue = number_format((float) $this->getOtherValue($tag), 2, '.', '');
+
+                        if($total>=$tagValue) {
+                            $total = $tagValue;
+                        }
+                        
+                    }
+                }
+
+                if ($operand['operand'] === '>') {
+                   
+                    if(!isset($total)) {
+                        $total=0;
+                    }
+                    
+                    foreach ($operand['tags'] as $tag) {
+
+                        $tagValue = number_format((float) $this->getOtherValue($tag), 2, '.', '');
+
+                        if($total<=$tagValue) {
+                            $total = $tagValue;
+                        }
+                        
+                    }
+                }
+
+                if ($operand['operand'] === 'count') {
+                   
+                    if(!isset($total)) {
+                        $total=0;
+                    }
+                    
+                    foreach ($operand['tags'] as $tag) {
+
+                        $tagValue = $this->getOtherValue($tag);
+
+                        if(isset($tagValue) && $tagValue != '' && $tagValue>0) {
+                            $total +=1 ;
+                        }
+                        
+                    }
+                }
+
+
+            }
+        }
+      
+   
+        $this->value = number_format((float) $total, 2, '.', '');
+        $this->save_value();
+        $wireModel = "formData.{$this->form_id}.{$this->name}";
+
+
+
+        $readonly = '';
+        if (isset($optionsArray['readonly'])) {
+            $readonly = (($optionsArray['readonly'] == true) ? 'disabled' : '');
+        }
+
+        $hidden = '';
+        if (isset($optionsArray['hidden'])) {
+            $hidden = (($optionsArray['hidden'] == true) ? 'style="display:none"' : '');
+        }
+
+
+        $data = '<div '.$hidden.' class="form-group col-sm-12 ' . ($this->config->class ?? "") . '">';
+
+
+        $data .= '<label style="display:inline-block">' . $this->config->title . '</label>';
+
+
+        // $data .= $this->generate_loading();
+
+
+        $data .= '<input ' . $readonly . ' wire:model.lazy="' . $wireModel . '" class="form-control " type="text" name="' . $this->name . '" ';
+
+        if ($this->config->required) {
+            $data .= ' required ';
+        }
+        $data .= ' value="' . ($this->value) . '">';
+
+        if (!$this->check_value()) {
+            $data .= '<div  class="invalid-feedback">' . $this->get_error_message() . '</div>';
+
+        } else {
+            if ($this->prediction) {
+                $data .= '<span class="prediction">Predicted by OCEER</span>';
+            }
+        }
+        $data .= '</div>';
+
+
+        return $data;
+
+    }
 }
