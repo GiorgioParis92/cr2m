851754ccf7a267883bc6d642ab63c1724ee63f41
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-09-30 13:47:00 +0200

    Update Db_text.php

--- app/FormModel/FormData/Db_text.php
+++ app/FormModel/FormData/Db_text.php
@@ -175,4 +175,133 @@ class Db_text extends AbstractFormData
     
         return true;
     }
+
+    public function render_pdf()
+    {
+        $jsonString = str_replace(["\n", '', "\r"], '', $this->config->options);
+        
+        $optionsArray = json_decode($jsonString, true);
+
+        $sql_command = $optionsArray['sql'];
+
+
+        if (isset($optionsArray['arguments'])) {
+            foreach ($optionsArray['arguments'] as $key => $data) {
+                $sql_command = str_replace($key, eval ($data), $sql_command);
+            }
+        }
+
+
+        $condition_valid = false;
+
+        if(isset($optionsArray['conditions'])) {
+          
+            foreach ($optionsArray as $key=>$condition_config) {
+                if($key=='conditions') {
+                    if ($this->check_condition($condition_config)) {
+                
+                        $condition_valid = true;
+        
+                        if (isset($condition_config['operation']) && $condition_config['operation'] == 'AND') {
+                            break;
+                        }
+        
+                    } else {
+                        $condition_valid = false; 
+                    }
+                }
+
+    
+            }
+        } else {
+            $condition_valid = true;
+        }
+
+
+  
+        if( $condition_valid == false) {
+            return '';
+        }
+
+
+        $class_prediction = '';
+
+        if (!$this->check_value()) {
+            $class_prediction = ' is-invalid';
+        }
+
+        $request = DB::select($sql_command);
+
+        if ($this->value && $this->value != '' && $this->value != 0) {
+
+            foreach ($request as $result) {
+                $fieldValue = $optionsArray['value'];
+                $fieldLabel = $optionsArray['label'];
+
+                if ($this->value > $result->$fieldLabel) {
+                    $this->value = $result->$fieldLabel;
+                    $this->save_value();
+                }
+
+            }
+
+        } else {
+            foreach ($request as $result) {
+                $fieldValue = $optionsArray['value'];
+                $fieldLabel = $optionsArray['label'];
+                $this->value = $result->$fieldLabel;
+                $this->save_value();
+            }
+        }
+
+
+        if(is_numeric($this->value)) {
+            $this->value=number_format($this->value, 2, ',', ' ');
+        }
+
+        $wireModel = "formData.{$this->form_id}.{$this->name}";
+
+        $readonly = '';
+        if (isset($optionsArray['readonly'])) {
+            $readonly = (($optionsArray['readonly'] == true) ? 'disabled' : '');
+        }
+
+        $wireModel = "formData.{$this->form_id}.{$this->name}";
+
+        $class_prediction = '';
+
+        if ($this->prediction) {
+            $class_prediction = ' prediction';
+        }
+        if (!$this->check_value()) {
+            $class_prediction = ' is-invalid';
+        }
+
+        $data = '<div class="form-group col-sm-12 ' . ($this->config->class ?? "") . '  group_' . $class_prediction . '">';
+
+        $data .= '<label style="display:inline-block">' . $this->config->title . '</label>';
+
+        $data .= $this->generate_loading();
+
+        $data .= '<input ' . $readonly . ' wire:model.lazy="' . $wireModel . '" class="form-control ' . $class_prediction . '" type="text" name="' . $this->name . '" ';
+
+        if ($this->config->required) {
+            $data .= ' required ';
+        }
+
+        $data .= ' value="' . ($this->value) . '">';
+
+        if (!$this->check_value()) {
+            $data .= '<div  class="invalid-feedback">' . $this->get_error_message() . '</div>';
+
+        } else {
+            if ($this->prediction) {
+                $data .= '<span class="prediction">Predicted by OCEER</span>';
+            }
+        }
+        $data .= '</div>';
+
+        return $data;
+
+    }
 }
