ac52ba3b8fb2fa2af4101168c91ea1a97a35183a
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-10-01 15:44:47 +0200

    Update DossierLivewire.php

--- app/Http/Livewire/DossierLivewire.php
+++ app/Http/Livewire/DossierLivewire.php
@@ -39,19 +39,19 @@ class DossierLivewire extends Component
         $this->time = now()->format('H:i:s');
 
 
-        $dossiers=Dossier::where('id','>',0)->get();
-        foreach($dossiers as $dossier) {
+        // $dossiers=Dossier::where('id','>',0)->get();
+        // foreach($dossiers as $dossier) {
 
-            if ($dossier && $dossier->etape) {
-                $orderColumn = $dossier->etape->order_column;
-            } else {
-                // Handle the case where $dossier or $dossier->etape is null
-                $orderColumn = null;
-            }
+        //     if ($dossier && $dossier->etape) {
+        //         $orderColumn = $dossier->etape->order_column;
+        //     } else {
+        //         // Handle the case where $dossier or $dossier->etape is null
+        //         $orderColumn = null;
+        //     }
 
-            $docs=getDocumentStatuses($dossier->id,$orderColumn);
+        //     $docs=getDocumentStatuses($dossier->id,$orderColumn);
 
-        }
+        // }
 
         // Fetch dossier with related data
         $this->dossier = Dossier::where('id', $id)
