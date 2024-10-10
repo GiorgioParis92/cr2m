010c98b2f9ec8a39a4521c5d97a8227e3e684085
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-10-10 16:43:35 +0200

    Update dossiers-table.blade.php

--- resources/views/livewire/dossiers-table.blade.php
+++ resources/views/livewire/dossiers-table.blade.php
@@ -646,7 +646,7 @@ span.badge.badge-outline-danger {
 
     function render_cell_mandataire(params) {
         const data = params.data;
-        console.log(data.mandataire)
+        console.log(data)
         if (!data || !data.mandataire || data.mandataire==0 || data.mandataire==null || data.mandataire==undefined) {
             return '';
         }
