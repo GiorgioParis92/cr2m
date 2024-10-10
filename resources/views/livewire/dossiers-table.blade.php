e5f4e7a2190ca88acb3c11f8b7e75cb5ae5a6f40
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-10-10 16:38:58 +0200

    a

--- resources/views/livewire/dossiers-table.blade.php
+++ resources/views/livewire/dossiers-table.blade.php
@@ -646,7 +646,7 @@ span.badge.badge-outline-danger {
 
     function render_cell_mandataire(params) {
         const data = params.data;
-        if (!data || !data.mandataire) {
+        if (!data || !data.mandataire || data.mandataire==0) {
             return '';
         }
         const imageUrl = data.mandataire_img ? `storage/${data.mandataire_img}` : null;
--- resources/views/partials/responseData.blade.php
+++ resources/views/partials/responseData.blade.php
@@ -45,7 +45,63 @@
                 @if (
                     (isset($data['initial_data']['display_title']) && $data['initial_data']['display_title'] != 'false') ||
                         !isset($data['initial_data']['display_title']))
-                    <h6 class="mb-0 =">{{ $tag }}</h6>
+                    <h6 class="mb-0 =">{{ <?php
+
+                        namespace App\Http\Livewire;
+                        
+                        use Livewire\Component;
+                        use Illuminate\Support\Facades\Http;
+                        use App\Models\Dossier;
+                        use App\Models\Client;
+                        
+                        class ResponseData extends Component
+                        {
+                            public $dossierId;
+                            public $dossier;
+                            public $responseData = null;
+                        
+                            public function mount($dossierId)
+                            {
+                                $this->dossierId = $dossierId;
+                                $this->dossier = Dossier::with('beneficiaire', 'fiche', 'etape', 'status','mar_client')->find($this->dossierId);
+                            }
+                        
+                            public function render()
+                            {
+                                return view('livewire.response-data');
+                            }
+                        
+                            public function loadResponseData()
+                            {
+                                $mar=Client::where('id',$this->dossier->mar)->first();
+                               
+                                if($mar) {
+                                    $login=$mar->anah_login;
+                                    $password=$mar->anah_password;
+                                }
+                                if ($this->dossier && $this->dossier->reference_unique) {
+                                    $url = url('/api/scrapping');
+                                    $token = 'qlcb1m8AlZU8dteqvYWFxrehJ2iGlGvUbinQhUNOa3yqjizldp0ARNiCDmsl';
+                        
+                                    $response = Http::withToken($token)
+                                        ->withHeaders(['Accept' => 'application/json'])
+                                        ->post($url, [
+                                            'reference_unique' => $this->dossier->reference_unique,
+                                            'login' => $login,
+                                        'password' => $password,
+                                        ]);
+                                     
+                                    if ($response->successful()) {
+                                        $this->responseData = $response->json();
+                                    } else {
+                                        $statusCode = $response->status();
+                                        $errorBody = $response->body();
+                                        $this->responseData = "Error ({$statusCode}): {$errorBody}";
+                                    }
+                                }
+                            }
+                        }
+                         }}</h6>
                 @endif
 
                 @foreach ($data['elements'] as $element)
