985bef4a97b22ecaf395d900eeae5847d2a767ab
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-10-21 18:02:15 +0200

    Update TypeEpaisseurVitre.php

--- app/Models/TypeEpaisseurVitre.php
+++ app/Models/TypeEpaisseurVitre.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TypeEpaisseursVitre extends Model
+class TypeEpaisseurVitre extends Model
 {
     use HasFactory;
 
-    protected $table = 'type_epaisseurs_vitre';
+    protected $table = 'type_epaisseur_vitre';
 
 }
