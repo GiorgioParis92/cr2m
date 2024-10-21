9d1fb611063c785755171e12d1ea2b66ab6973dc
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-10-21 15:51:31 +0200

    a

---
+++ app/Http/Controllers/Api/DynamicModelController.php
@@ -0,0 +1,99 @@
+<?php 
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Str;
+
+class DynamicModelController extends \App\Http\Controllers\Controller
+{
+    // Resolve the model name dynamically
+    protected function getModelInstance($modelName)
+    {
+        $modelClass = 'App\\Models\\' . Str::studly($modelName);
+        
+        if (!class_exists($modelClass)) {
+            throw new ModelNotFoundException("Model $modelName not found.");
+        }
+
+        return new $modelClass;
+    }
+
+    // Fetch all records with dynamic filtering
+    public function index(Request $request, $modelName)
+    {
+
+        
+        $model = $this->getModelInstance($modelName);
+        $query = $model::query();
+
+        // Apply filters dynamically based on request
+        foreach ($request->all() as $field => $value) {
+            if ($model->isFillable($field)) {
+                $query->where($field, $value);
+            }
+        }
+
+        // Apply pagination or return all results
+        if ($request->has('paginate')) {
+            return response()->json($query->paginate($request->input('paginate')));
+        }
+
+        return response()->json($query->get());
+    }
+
+    // Fetch specific record by ID
+    public function show($modelName, $id)
+    {
+        $model = $this->getModelInstance($modelName);
+        $record = $model::findOrFail($id);
+        return response()->json($record);
+    }
+
+    // Create a new record
+    public function store(Request $request, $modelName)
+    {
+        $model = $this->getModelInstance($modelName);
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:100', // Customize validation as needed
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        $record = $model::create($request->all());
+        return response()->json($record, 201);
+    }
+
+    // Update a record
+    public function update(Request $request, $modelName, $id)
+    {
+        $model = $this->getModelInstance($modelName);
+        $record = $model::findOrFail($id);
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:100', // Customize validation as needed
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        $record->update($request->all());
+        return response()->json($record);
+    }
+
+    // Delete a record
+    public function destroy($modelName, $id)
+    {
+        $model = $this->getModelInstance($modelName);
+        $record = $model::findOrFail($id);
+        $record->delete();
+        return response()->json(null, 204);
+    }
+}
+
--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,9 @@ use App\Http\Controllers\Api\UpdatePermission;
 use App\Http\Controllers\Api\ClientController;
 use App\Http\Controllers\Api\TablesController;
 
+use App\Http\Controllers\Api\DynamicModelController;
+
+
 
 
 
@@ -91,86 +94,93 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/dossiers-rdv', [DossiersController::class, 'index']);
     Route::post('/dossiers-rdv', [DossiersController::class, 'index']);
 
-    Route::get('/beneficiaires', [BeneficiaireController::class, 'index']);
-    Route::get('/beneficiaires/{id}', [BeneficiaireController::class, 'show']);
-    Route::post('/beneficiaires', [BeneficiaireController::class, 'store']);
-    Route::put('/beneficiaires/{id}', [BeneficiaireController::class, 'update']);
-    Route::delete('/beneficiaires/{id}', [BeneficiaireController::class, 'destroy']);
+//     Route::get('/beneficiaires', [BeneficiaireController::class, 'index']);
+//     Route::get('/beneficiaires/{id}', [BeneficiaireController::class, 'show']);
+//     Route::post('/beneficiaires', [BeneficiaireController::class, 'store']);
+//     Route::put('/beneficiaires/{id}', [BeneficiaireController::class, 'update']);
+//     Route::delete('/beneficiaires/{id}', [BeneficiaireController::class, 'destroy']);
 
-    Route::get('/dossiers_data', [DossiersDataController::class, 'index']);
-    Route::get('/dossiers_data/{id}', [DossiersDataController::class, 'show']);
-    Route::post('/dossiers_data', [DossiersDataController::class, 'store']);
-    Route::put('/dossiers_data/{id}', [DossiersDataController::class, 'update']);
-    Route::delete('/dossiers_data/{id}', [DossiersDataController::class, 'destroy']);
-
-
-    Route::get('/dossiers', [Dossiers::class, 'index']);
-    Route::get('/dossiers/{id}', [Dossiers::class, 'show']);
-    Route::post('/dossiers', [Dossiers::class, 'store']);
-    Route::put('/dossiers/{id}', [Dossiers::class, 'update']);
-    Route::delete('/dossiers/{id}', [Dossiers::class, 'destroy']);
-
-    Route::get('/etapes', [EtapesController::class, 'index']);
-    Route::get('/etapes/{id}', [EtapesController::class, 'show']);
-    Route::post('/etapes', [EtapesController::class, 'store']);
-    Route::put('/etapes/{id}', [EtapesController::class, 'update']);
-    Route::delete('/etapes/{id}', [EtapesController::class, 'destroy']);
+//     Route::get('/dossiers_data', [DossiersDataController::class, 'index']);
+//     Route::get('/dossiers_data/{id}', [DossiersDataController::class, 'show']);
+//     Route::post('/dossiers_data', [DossiersDataController::class, 'store']);
+//     Route::put('/dossiers_data/{id}', [DossiersDataController::class, 'update']);
+//     Route::delete('/dossiers_data/{id}', [DossiersDataController::class, 'destroy']);
 
 
-    Route::get('/fiches', [FicheController::class, 'index']);
-Route::get('/fiches/{id}', [FicheController::class, 'show']);
-Route::post('/fiches', [FicheController::class, 'store']);
-Route::put('/fiches/{id}', [FicheController::class, 'update']);
-Route::delete('/fiches/{id}', [FicheController::class, 'destroy']);
+//     Route::get('/dossiers', [Dossiers::class, 'index']);
+//     Route::get('/dossiers/{id}', [Dossiers::class, 'show']);
+//     Route::post('/dossiers', [Dossiers::class, 'store']);
+//     Route::put('/dossiers/{id}', [Dossiers::class, 'update']);
+//     Route::delete('/dossiers/{id}', [Dossiers::class, 'destroy']);
 
-Route::get('/forms', [FormController::class, 'index']);
-Route::get('/forms/{id}', [FormController::class, 'show']);
-Route::post('/forms', [FormController::class, 'store']);
-Route::put('/forms/{id}', [FormController::class, 'update']);
-Route::delete('/forms/{id}', [FormController::class, 'destroy']);
-
-
-
-Route::get('/forms_config', [FormsConfigController::class, 'index']);
-Route::get('/forms_config/{id}', [FormsConfigController::class, 'show']);
-Route::post('/forms_config', [FormsConfigController::class, 'store']);
-Route::put('/forms_config/{id}', [FormsConfigController::class, 'update']);
-Route::delete('/forms_config/{id}', [FormsConfigController::class, 'destroy']);
-
-Route::get('/forms_data', [FormsDataController::class, 'index']);
-Route::get('/forms_data/{id}', [FormsDataController::class, 'show']);
-Route::post('/forms_data', [FormsDataController::class, 'store']);
-Route::put('/forms_data/{id}', [FormsDataController::class, 'update']);
-Route::delete('/forms_data/{id}', [FormsDataController::class, 'destroy']);
-
-Route::get('/rdv', [RdvControllerPhone::class, 'index']);
-Route::get('/rdv/{id}', [RdvControllerPhone::class, 'show']);
-Route::post('/rdv', [RdvControllerPhone::class, 'store']);
-Route::put('/rdv/{id}', [RdvControllerPhone::class, 'update']);
-Route::delete('/rdv/{id}', [RdvControllerPhone::class, 'destroy']);
+//     Route::get('/etapes', [EtapesController::class, 'index']);
+//     Route::get('/etapes/{id}', [EtapesController::class, 'show']);
+//     Route::post('/etapes', [EtapesController::class, 'store']);
+//     Route::put('/etapes/{id}', [EtapesController::class, 'update']);
+//     Route::delete('/etapes/{id}', [EtapesController::class, 'destroy']);
 
 
-Route::get('/rdv_status', [RdvStatusController::class, 'index']);
-Route::get('/rdv_status/{id}', [RdvStatusController::class, 'show']);
-Route::post('/rdv_status', [RdvStatusController::class, 'store']);
-Route::put('/rdv_status/{id}', [RdvStatusController::class, 'update']);
-Route::delete('/rdv_status/{id}', [RdvStatusController::class, 'destroy']);
+//     Route::get('/fiches', [FicheController::class, 'index']);
+// Route::get('/fiches/{id}', [FicheController::class, 'show']);
+// Route::post('/fiches', [FicheController::class, 'store']);
+// Route::put('/fiches/{id}', [FicheController::class, 'update']);
+// Route::delete('/fiches/{id}', [FicheController::class, 'destroy']);
 
-Route::get('/rdv_type', [RdvTypeController::class, 'index']);
-Route::get('/rdv_type/{id}', [RdvTypeController::class, 'show']);
-Route::post('/rdv_type', [RdvTypeController::class, 'store']);
-Route::put('/rdv_type/{id}', [RdvTypeController::class, 'update']);
-Route::delete('/rdv_type/{id}', [RdvTypeController::class, 'destroy']);
+// Route::get('/forms', [FormController::class, 'index']);
+// Route::get('/forms/{id}', [FormController::class, 'show']);
+// Route::post('/forms', [FormController::class, 'store']);
+// Route::put('/forms/{id}', [FormController::class, 'update']);
+// Route::delete('/forms/{id}', [FormController::class, 'destroy']);
 
 
-Route::get('/clients', [ClientController::class, 'index']);
-Route::get('/clients/{id}', [ClientController::class, 'show']);
-Route::post('/clients', [ClientController::class, 'store']);
-Route::put('/clients/{id}', [ClientController::class, 'update']);
-Route::delete('/clients/{id}', [ClientController::class, 'destroy']);
+
+// Route::get('/forms_config', [FormsConfigController::class, 'index']);
+// Route::get('/forms_config/{id}', [FormsConfigController::class, 'show']);
+// Route::post('/forms_config', [FormsConfigController::class, 'store']);
+// Route::put('/forms_config/{id}', [FormsConfigController::class, 'update']);
+// Route::delete('/forms_config/{id}', [FormsConfigController::class, 'destroy']);
+
+// Route::get('/forms_data', [FormsDataController::class, 'index']);
+// Route::get('/forms_data/{id}', [FormsDataController::class, 'show']);
+// Route::post('/forms_data', [FormsDataController::class, 'store']);
+// Route::put('/forms_data/{id}', [FormsDataController::class, 'update']);
+// Route::delete('/forms_data/{id}', [FormsDataController::class, 'destroy']);
+
+// Route::get('/rdv', [RdvControllerPhone::class, 'index']);
+// Route::get('/rdv/{id}', [RdvControllerPhone::class, 'show']);
+// Route::post('/rdv', [RdvControllerPhone::class, 'store']);
+// Route::put('/rdv/{id}', [RdvControllerPhone::class, 'update']);
+// Route::delete('/rdv/{id}', [RdvControllerPhone::class, 'destroy']);
+
+
+// Route::get('/rdv_status', [RdvStatusController::class, 'index']);
+// Route::get('/rdv_status/{id}', [RdvStatusController::class, 'show']);
+// Route::post('/rdv_status', [RdvStatusController::class, 'store']);
+// Route::put('/rdv_status/{id}', [RdvStatusController::class, 'update']);
+// Route::delete('/rdv_status/{id}', [RdvStatusController::class, 'destroy']);
+
+// Route::get('/rdv_type', [RdvTypeController::class, 'index']);
+// Route::get('/rdv_type/{id}', [RdvTypeController::class, 'show']);
+// Route::post('/rdv_type', [RdvTypeController::class, 'store']);
+// Route::put('/rdv_type/{id}', [RdvTypeController::class, 'update']);
+// Route::delete('/rdv_type/{id}', [RdvTypeController::class, 'destroy']);
+
+
+// Route::get('/clients', [ClientController::class, 'index']);
+// Route::get('/clients/{id}', [ClientController::class, 'show']);
+// Route::post('/clients', [ClientController::class, 'store']);
+// Route::put('/clients/{id}', [ClientController::class, 'update']);
+// Route::delete('/clients/{id}', [ClientController::class, 'destroy']);
 
 Route::post('/update_permission', [UpdatePermission::class, 'index']);
 
 Route::get('/tables', [TablesController::class, 'index']);
+
+// Dynamic routes for any model
+Route::get('/{model}', [DynamicModelController::class, 'index']);
+Route::get('/{model}/{id}', [DynamicModelController::class, 'show']);
+Route::post('/{model}', [DynamicModelController::class, 'store']);
+Route::put('/{model}/{id}', [DynamicModelController::class, 'update']);
+Route::delete('/{model}/{id}', [DynamicModelController::class, 'destroy']);
 
 });
