b306469864bb55eace88537e9c8bf003bc519e29
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-10-29 19:53:46 +0100

    Update UserPosition.php

--- app/Models/UserPosition.php
+++ app/Models/UserPosition.php
@@ -8,7 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class UserPosition extends Model
 {
     use HasFactory;
+    protected $fillable = [
 
+        'user_id',
+        'lat',
+        'lng'
+    ];
     protected $table = 'user_position';
 
 }
