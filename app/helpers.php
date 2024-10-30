b43e13e105f01a83ca421f15574b0f5e29510da5
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-10-30 10:12:23 +0100

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -690,6 +690,8 @@ if (!function_exists('change_status')) {
     function change_status($key, $value, $dossier_id)
     {
   
+        Dossier::where('id', $dossier_id)->update([ 'status_id' => $value]);
+
     if($key=='status_id' && $value==15) {
         Dossier::where('id', $dossier_id)->update(['etape_number' => 1, 'status_id' => $value]);
         Dossier::where('id', $dossier_id)->update(['annulation' => 1, 'status_id' => $value]);
