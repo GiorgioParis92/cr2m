99857e902eaa0ffbcc499e42b19d359657c6a38a
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-11-05 20:03:57 +0100

    Update PDFController.php

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -200,7 +200,7 @@ class PDFController extends Controller
             $dossier = Dossier::with('mar')->where('folder', $validated['dossier_id'])->first();
 
         }
-
+dd($dossier);
         $dossierId = $dossier->id;
         $folderPath = "public/dossiers/{$dossier->folder}";
         $directPath = "dossiers/{$dossier->folder}";
