515c59b49214bae90369efac5452b067be83818a
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-11-10 00:24:50 +0100

    Update notifications.blade.php

--- resources/views/livewire/notifications.blade.php
+++ resources/views/livewire/notifications.blade.php
@@ -13,33 +13,20 @@
             }
 
             function createNotifications() {
-                Push.create("Hello Giorgio!", {
-                    body: "Welcome to the Dashboard.",
+                Push.create("Hello {{auth()->user()->name}}!", {
+                    body: "Bienvenue sur le CRM ATLAS.",
                     timeout: 5000,
                     icon: iconPath,
                     tag: 'notification-1',
                     onClick: function () {
                         // Open custom link when notification is clicked
-                        window.open('https://example.com/your-custom-link-1', '_blank');
+                        window.open('https://crm.genius-market.fr', '_blank');
                         // Close the notification
                         this.close();
                     }
                 });
 
-                setTimeout(() => {
-                    Push.create("Hello Giorgio2!", {
-                        body: "Check out our new features.",
-                        timeout: 5000,
-                        icon: iconPath,
-                        tag: 'notification-2',
-                        onClick: function () {
-                            // Open another custom link when notification is clicked
-                            window.open('https://example.com/your-custom-link-2', '_blank');
-                            // Close the notification
-                            this.close();
-                        }
-                    });
-                }, 100); // Slight delay
+               
             }
         });
     </script>
