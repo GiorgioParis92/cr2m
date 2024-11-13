81140235c07a839a31d746deb880303bd3ebdd20
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-11-13 17:05:11 +0100

    Update PDFController.php

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -375,9 +375,9 @@ class PDFController extends Controller
                             $signature_client = $client->signature ?? '';
 
                             $signaturePath = storage_path('app/public/' . $signature_client);
-                            if(auth()->user()->id==1) {
+                  
                                 dd($signature);
-                            }
+                            
                             
                             if (file_exists($signaturePath)) {
                                 // Set default position and size if not provided
