7b21c9dd17eade8d9c21a1c33ffc37d90049dc95
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-11-19 11:27:41 +0100

    a

--- app/Models/FormConfig.php
+++ app/Models/FormConfig.php
@@ -9,5 +9,23 @@ class FormConfig extends Model
 {
     use HasFactory;
 
+    // Specify the table name if it doesn't follow Laravel's naming convention
     protected $table = 'forms_config';
+
+    // If your table doesn't have timestamp columns, disable them
+    public $timestamps = false;
+
+    // Allow mass assignment on these attributes
+    protected $fillable = [
+        'form_id',
+        'ordering',
+        // Add other columns you want to be mass assignable
+    ];
+
+    // Optionally, cast attributes to appropriate data types
+    protected $casts = [
+        'form_id' => 'integer',
+        'ordering' => 'integer',
+        // Add other casts as needed
+    ];
 }
