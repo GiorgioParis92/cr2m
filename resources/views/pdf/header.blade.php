0243877d0d7b1ae8f662b28255204be231446b14
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-12-24 12:07:36 +0100

    Update header.blade.php

--- resources/views/pdf/header.blade.php
+++ resources/views/pdf/header.blade.php
@@ -1,6 +1,8 @@
 <page_header style="font-family:dejavusans">
 
         <div class="logo_header" style="">
+            @dump($dossier->client->main_logo ?? '')
+            {{$dossier->client->main_logo ?? ''}}
             @if(isset($dossier->client->main_logo) && file_exists(storage_path('app/public/' . $dossier->client->main_logo)))
 
             <img src="{{ asset('storage/' . $dossier->client->main_logo) }}" alt="Logo">
