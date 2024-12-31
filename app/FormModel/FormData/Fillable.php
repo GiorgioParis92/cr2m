1658ad2c814fd2cb06df5c97f50202c45e6f3cca
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2024-12-31 10:07:02 +0100

    Update Fillable.php

--- app/FormModel/FormData/Fillable.php
+++ app/FormModel/FormData/Fillable.php
@@ -225,9 +225,16 @@ class Fillable extends AbstractFormData
 
 
         $data .= '<td class="align-middle text-sm">';
-        $data .= '<form action="' . route("upload_file", ["form_id" => $this->form_id, "folder" => "dossiers", "upload_image" => "dossiers", "clientId" => $this->dossier->folder, "template" => $this->name]) . '" class="dropzone dropzone_button bg-primary" id="dropzone-' . $this->name . '">';
+        $data .= '<form method="post" action="' . route("upload_file", ["form_id" => $this->form_id, "folder" => "dossiers", "upload_image" => "dossiers", "clientId" => ($this->dossier->folder ?? $this->dossier_id), "template" => $this->name,'config'=>$this]) . '" class="dropzone dropzone_button bg-primary" id="dropzone-' . $this->name . '">';
         $data .= csrf_field(); // This will generate the CSRF token input field
+        $data .= '<input type="hidden" name="template" value="'.$this->name.'">';
+        $data .= '<input type="hidden" name="dossier_id" value="'.$this->dossier_id.'">';
+        $data .= '<input type="hidden" name="form_id" value="'.$this->form_id.'">';
+        $data .= '<input type="hidden" name="clientId" value="'.($this->dossier->folder ?? $this->dossier_id).'">';
+        $data .= '<input type="hidden" name="random_name" value="false">';
+        $data .= '<input type="hidden" name="upload_image" value="true">';
         $data .= '<div class="dz-message"><i class="fas fa-arrow-up"></i> Upload';
+
         $data .= '</div>';
         $data .= '</form>';
 
