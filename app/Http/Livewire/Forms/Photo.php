5de34c6049744ed8888795c5a797fc2effdc707e
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2025-01-03 14:28:59 +0100

    Update Photo.php

--- app/Http/Livewire/Forms/Photo.php
+++ app/Http/Livewire/Forms/Photo.php
@@ -29,12 +29,13 @@ class Photo extends AbstractData
     public function convertHeicToJpg($filePath)
     {
         $heicPath = storage_path("app/public/{$filePath}");
-        dump($heicPath);
+     
         if (!file_exists($heicPath)) {
             return $filePath; // Return original if file doesn't exist
         }
        
         $extension = pathinfo($filePath, PATHINFO_EXTENSION);
+        dump($extension);
         if (strtolower($extension) !== 'heic') {
             return $filePath; // Return original if not HEIC
         }
