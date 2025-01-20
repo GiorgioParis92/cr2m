d0d9e0f005f7a1b19d37f874dd925c2e9abcad22
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2025-01-20 15:24:42 +0100

    Update Textarea.php

--- app/FormModel/FormData/Textarea.php
+++ app/FormModel/FormData/Textarea.php
@@ -10,53 +10,34 @@ class Textarea extends AbstractFormData
     {
         $wireModel = "formData.{$this->form_id}.{$this->name}";
 
-        $data = '<div class="form-group  col-sm-12 '.($this->config->class ?? "").'">';
-        $data .= '<label>'.($this->config->title ?? '').'</label>';
-        $data .= '<textarea  wire:blur="update_value(\''.$wireModel.'\',  $event.target.value)" class="form-control" name="'.($this->config->name ?? '').'">'.($this->value ?? "") .'</textarea>';
-
+        $data = '<div class="form-group col-sm-12 ' . ($this->config->class ?? "") . '">';
+        $data .= '<label>' . ($this->config->title ?? '') . '</label>';
+        $data .= '<textarea wire:blur="update_value(\'' . $wireModel . '\', $event.target.value)" class="form-control" name="' . ($this->config->name ?? '') . '">' . ($this->value ?? "") . '</textarea>';
         $data .= '</div>';
-
 
         return $data;
     }
 
     public function render_form(bool $is_error)
     {
-
-
-        $data = '<div class="form-group  col-sm-12 '.($this->config->class ?? "").'">';
-        $data .= '<label>'.($this->config->title ?? '').'</label>';
-        $data .= '<div  class="form-control" name="'.($this->config->name ?? '').'">'.($this->value ?? "") .'</div>';
-
+        $data = '<div class="form-group col-sm-12 ' . ($this->config->class ?? "") . '">';
+        $data .= '<label>' . ($this->config->title ?? '') . '</label>';
+        $data .= '<div class="form-control" name="' . ($this->config->name ?? '') . '">' . ($this->value ?? "") . '</div>';
         $data .= '</div>';
-
 
         return $data;
     }
 
     public function render_pdf()
     {
-        $data='ok';
-  
-        // if(!$this->value || $this->value=='') {
-        //     return false;
-        // }
+        if (!$this->value || $this->value == '') {
+            return false;
+        }
 
-        // $data = '<div  class="form-group col-sm-12 ' . ($this->config->class ?? "") . '">';
-
-
-        // $data .= '<div class="s3" style="display:block;margin-top:15px;margin-bottom:8px">' . $this->config->title . '</div>';
-
-
-
-
-        // $data .= '<div style="display:block;margin-bottom:8px">'.$this->value.'</div>';
-
-
-
-
-        // $data .= '</div>';
-
+        $data = '<div class="form-group col-sm-12 ' . ($this->config->class ?? "") . '">';
+        $data .= '<div class="s3" style="display:block;margin-top:15px;margin-bottom:8px">' . $this->config->title . '</div>';
+        $data .= '<div style="display:block;margin-bottom:8px">' . $this->value . '</div>';
+        $data .= '</div>';
 
         return $data;
     }
