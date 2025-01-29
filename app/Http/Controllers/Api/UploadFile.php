c86d433c35c3a50f98d53c62f0b6470684ce830c
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2025-01-29 12:49:35 +0100

    Update UploadFile.php

--- app/Http/Controllers/Api/UploadFile.php
+++ app/Http/Controllers/Api/UploadFile.php
@@ -81,6 +81,7 @@ class UploadFile extends \App\Http\Controllers\Controller
             // Set correct permissions for the temporary thumbnail
             chmod($tempThumbnailPath, 0775);
 
+            
             // Rename the temporary thumbnail to the original file name
             $finalThumbnailPath = storage_path('app/public/' . $directory . '/' . $fileName);
             rename($tempThumbnailPath, $finalThumbnailPath);
