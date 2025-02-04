2b77fcc9955373e1695a0e7c413de01b2d63f84b
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2025-02-04 13:48:00 +0100

    Update textarea.blade.php

--- resources/views/livewire/forms/textarea.blade.php
+++ resources/views/livewire/forms/textarea.blade.php
@@ -1,18 +1,19 @@
 <div class="col-sm-12 {{ $conf['class'] ?? 'col-lg-12' }}">
     @if($check_condition)
-    <label>{{ $conf['title'] ?? '' }}</label>
-    <textarea
-        style="min-height:100px"
-        wire:ignore.self
-        name="{{ $conf['name'] }}"
-        class="form-control"
-        wire:model.debounce.500ms="value"
-        placeholder=""
-        onfocus="this.style.height='auto'; this.style.height=(this.scrollHeight)+'px';"
-        oninput="this.style.height='auto'; this.style.height=(this.scrollHeight)+'px';"
-    >{{ $value }}</textarea>
-    @error('value')
-        <span class="text-danger">{{ $message }}</span>
-    @enderror
+        <label>{{ $conf['title'] ?? '' }}</label>
+        <textarea
+            name="{{ $conf['name'] }}"
+            class="form-control"
+            style="min-height: 100px;"
+            wire:ignore.self
+            wire:model.lazy="value"
+            placeholder=""
+            onfocus="this.style.height = 'auto'; this.style.height = (this.scrollHeight) + 'px';"
+            oninput="this.style.height = 'auto'; this.style.height = (this.scrollHeight) + 'px';"
+        >{{ $value }}</textarea>
+        
+        @error('value')
+            <span class="text-danger">{{ $message }}</span>
+        @enderror
     @endif
 </div>
