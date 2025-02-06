c31204fdc888801882842c1779618a43d36719bc
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2025-02-06 16:01:18 +0100

    Update Photo.php

--- app/FormModel/FormData/Photo.php
+++ app/FormModel/FormData/Photo.php
@@ -193,8 +193,9 @@ class Photo extends AbstractFormData
         foreach ($values as $value) {
             $filePath = storage_path('app/public/' . $value);
     
-            $thumbnailPath = preg_replace('/(\.\w+)$/', '_thumbnail$1', $value); // Append '_thumbnail' before the file extension
-
+            $pathInfo = pathinfo($value);
+            $thumbnailPath = $pathInfo['dirname'] . '/' . $pathInfo['filename'] . '_thumbnail.' . $pathInfo['extension'];
+            
             if (Storage::exists('public/' . $thumbnailPath)) {
                 $filePath = storage_path('app/public/' . $thumbnailPath);
             } else {
