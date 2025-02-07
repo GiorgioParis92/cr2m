c0b3dd47c6568acba18da6341b661f8f3036f4fc
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2025-02-07 01:41:09 +0100

    Update audio.blade.php

--- resources/views/livewire/forms/audio.blade.php
+++ resources/views/livewire/forms/audio.blade.php
@@ -30,7 +30,7 @@
         <div class="mt-3">
             {{ storage_path($value) }}
             @if (!empty($value))
-                <audio id="audioPlayback" controls class="w-100" src="{{ storage_path($value) }}"></audio>
+                <audio id="audioPlayback" controls class="w-100" src="{{ ($value) }}"></audio>
             @else
                 <audio id="audioPlayback" controls style="display: none;" class="w-100"></audio>
             @endif
