4d1d243ba48fbe29b5c88621268916294e555898
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2025-02-07 02:45:13 +0100

    Update Audio.php

--- app/Http/Livewire/Forms/Audio.php
+++ app/Http/Livewire/Forms/Audio.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire\Forms;
 
+use App\Models\FormsData;
+
 
 class Audio extends AbstractData
 {
@@ -10,6 +12,12 @@ class Audio extends AbstractData
 
     public function mount($conf, $form_id, $dossier_id) {
         parent::mount($conf, $form_id, $dossier_id);
+
+        $pdf=FormsData::where('dossier_id',$dossier_id)
+        ->where('form_id',$form_id)
+        ->where('meta_key',$conf['name'].'_pdf')->first();
+
+        $this->pdf = $pdf->meta_value ?? false;
     }
 
     public function getErrorMessage() {
