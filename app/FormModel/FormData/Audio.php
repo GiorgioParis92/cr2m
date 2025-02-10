6a114b2ffb1cef46d95f0f9fe7f87fbfa34b3c79
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2025-02-10 19:59:30 +0100

    a

---
+++ app/FormModel/FormData/Audio.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\FormModel\FormData;
+
+use Illuminate\Support\Facades\DB;
+use App\Models\Dossier;
+
+class Audio extends AbstractFormData
+{
+    
+    public function render(bool $is_error)
+    {
+     
+        $data='';
+     
+        return $data;
+    }
+
+
+    
+    public function check_value() {
+     
+ 
+
+        return true;
+    }
+
+
+    public function get_error_message()
+    {
+ 
+        return '';
+    }
+
+ 
+    public function render_pdf()
+    {
+        if(!$this->value) {
+            return false; 
+         }
+        $data='';
+  
+        
+        return $data;
+    }
+
+    public function check_condition($condition_config)
+    {
+
+    
+        return true;
+    }
+
+}
