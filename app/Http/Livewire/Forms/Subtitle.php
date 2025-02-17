506f067a82716478970ff80691cfa68e10612a80
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2025-02-17 11:54:44 +0100

    a

---
+++ app/Http/Livewire/Forms/Subtitle.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Livewire\Forms;
+
+use Livewire\Component;
+use App\Models\{
+    Dossier,
+    Etape,
+    DossiersActivity,
+    User,
+    Form,
+    Forms,
+    FormConfig,
+    Rdv,
+    RdvStatus,
+    Client,
+    FormsData,
+    Card
+};
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+class Subtitle extends AbstractData
+{
+    public function mount($conf, $form_id, $dossier_id) {
+        parent::mount($conf, $form_id, $dossier_id);
+    }
+
+    public function getErrorMessage() {
+        return '';
+    }
+
+
+    protected function validateValue($value): bool
+    {
+
+        return true;
+    }
+    public function render()
+    {
+
+        return view('livewire.forms.subtitle');
+    }
+}
---
+++ resources/views/livewire/forms/subtitle.blade.php
@@ -0,0 +1,13 @@
+<div class="col-sm-12 {{ $conf['class'] ?? 'col-lg-12' }}">
+ 
+    @if($check_condition)
+    <div class="card-header">
+        <h5>{{ $conf['title'] ?? '' }}</h5>
+    </div>
+    @endif
+
+
+
+</div>
+
+
