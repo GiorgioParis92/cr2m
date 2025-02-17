8481b3f1b4cbadf0b8fa20ccb168b88e47092644
GiorgioParis92 <36301653+GiorgioParis92@users.noreply.github.com>
2025-02-17 18:43:40 +0100

    Update dossier-livewire-new.blade.php

--- resources/views/livewire/dossier-livewire-new.blade.php
+++ resources/views/livewire/dossier-livewire-new.blade.php
@@ -455,7 +455,7 @@
                                                     <div class="accordion-item card mt-4 pl-4 pr-4 pb-3" >
                                                         <h2 class="accordion-header"
                                                             id="heading-{{ $groupId }}">
-                                                            <button wire:ignore.self
+                                                            <button 
                                                             class="accordion-button @if($expandedTitleId !== $groupId) collapsed @endif"
                                                             type="button"
                                                             {{-- wire:click="toggleTitle('{{ $groupId }}')" --}}
@@ -466,7 +466,7 @@
                                                             {{ $group[0]['title'] ?? 'Untitled' }}
                                                         </button>
                                                         </h2>
-                                                        <div wire:ignore.self id="collapse-{{ $groupId }}"
+                                                        <div  id="collapse-{{ $groupId }}"
                                                             class="accordion-collapse collapse @if ($expandedTitleId === $groupId) show @endif"
                                                             aria-labelledby="heading-{{ $groupId }}"
                                                             data-bs-parent="#accordionForm">
