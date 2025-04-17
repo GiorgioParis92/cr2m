75a1654182587d86d4a11c0dada3224e2fe151b3
GiorgioParis92 <gkalfon@hotmail.com>
2025-04-17 10:59:36 +0200

    m

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -851,7 +851,7 @@ class PDFController extends Controller
                         } else {
                             if ($element->type != 'table') {
                                 if ($element->type == 'photo') {
-                                    $title_content .= '<tr><td style="width:100%;border:1px solid #ccc;border-collapse: collapse;padding-left:12px;padding-bottom:15px">Photos</td></tr></table>';
+                                    $title_content .= '<tr><td style="width:100%;border:1px solid #ccc;border-collapse: collapse;padding-left:12px;padding-bottom:15px">Photos</td></tr>';
                                 } else {
                                     $title_content .= '<tr><td style="width:100%;border:1px solid #ccc;border-collapse: collapse;padding-left:12px;padding-bottom:15px">' . $instance_result . '</td></tr>';
 
