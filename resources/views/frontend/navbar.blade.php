57f857e68c66375ef54fd470de5ecf4643ef1d30
GiorgioParis92 <gkalfon@hotmail.com>
2025-06-05 11:31:58 +0200

    m

--- resources/views/frontend/navbar.blade.php
+++ resources/views/frontend/navbar.blade.php
@@ -112,12 +112,7 @@
                         <span class="nav-link-text ms-1"><i class="fa fa-users"></i>  Statistiques</span>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link  " href="{{ route('reporting') }}">
-
-                        <span class="nav-link-text ms-1"><i class="fa fa-users"></i>  Reporting</span>
-                    </a>
-                </li>
+      
                     <li class="nav-item">
                         <a class="nav-link  " href="{{ route('users.index') }}">
 
@@ -159,6 +154,12 @@
                         </li>
 
                     </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link  " href="{{ route('reporting') }}">
+
+                        <span class="nav-link-text ms-1"><i class="fa fa-users"></i>  Reporting</span>
+                    </a>
                 </li>
                 @endif
             </ul>
