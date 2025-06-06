7d0eb5d1da4c060e345fe2d03a651860c6e0be19
GiorgioParis92 <gkalfon@hotmail.com>
2025-06-06 13:13:51 +0200

    m

--- resources/views/templates/dossier_juridique.blade.php
+++ resources/views/templates/dossier_juridique.blade.php
@@ -192,15 +192,7 @@
 <page backtop="1mm" backleft="10mm" backright="10mm" backimg="" backbottom="10mm">
 
     @include('pdf.footer')
-    <div class="row">
-        <div class="col-12" style="max-width:40%;margin:auto;text-align:center;margin-top:-30px">
-            @if(isset($dossier->mar_client->main_logo) && file_exists(storage_path('app/public/' . $dossier->mar_client->main_logo)))
 
-                <img src="{{ asset('storage/' . $dossier->mar_client->main_logo) }}" alt="Logo">
-
-            @endif
-        </div>
-    </div>
 
     <h1 class="text-center">Dossier juridique – Mandataire financier et administratif Anah</h1>
 
@@ -314,15 +306,7 @@
 <page backtop="1mm" backleft="10mm" backright="10mm" backimg="" backbottom="10mm">
 
     @include('pdf.footer')
-    <div class="row">
-        <div class="col-12" style="max-width:40%;margin:auto;text-align:center;margin-top:-30px">
-            @if(isset($dossier->mar_client->main_logo) && file_exists(storage_path('app/public/' . $dossier->mar_client->main_logo)))
 
-                <img src="{{ asset('storage/' . $dossier->mar_client->main_logo) }}" alt="Logo">
-
-            @endif
-        </div>
-    </div>
     <div class="my-4">
         <h3>Article 5 Quater: Exclusion de responsabilité du Mandataire </h3>
 
